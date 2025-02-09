0e03db432ae42625ddf376935cc422a3049504fc
gandib <55914763+gandib@users.noreply.github.com>
2025-02-09 21:55:01 +0600

    Problems completed

--- index.js
+++ index.js
@@ -15,6 +15,15 @@ const getIndex = (nums, target) => {
 getIndex([1, 4, 5], 6);
 
 // Solution 2
+
+// Input: nums = [-1,0,1,2,-1,-4]
+// Output: [[-1,-1,2],[-1,0,1]]
+// nums[0] + nums[1] + nums[2] = (-1) + 0 + 1 = 0.
+// nums[1] + nums[2] + nums[4] = 0 + 1 + (-1) = 0.
+// nums[0] + nums[3] + nums[4] = (-1) + 2 + (-1) = 0.
+// The distinct triplets are [-1,0,1] and [-1,-1,2].
+// Notice that the order of the output and the order of the triplets does not matter.
+// console.log("-1,0,-1" === "1,0,-1");
 const getTriplets = (nums) => {
   const triplets = [];
   for (let i = 0; i < nums.length; i++) {
@@ -55,36 +64,6 @@ const nums = [2, 1, 0, 1, 3, 3];
 nums.sort((a, b) => a - b);
 let k = removeDuplicates(nums);
 console.log(k, `nums =`, nums.slice(0, k));
-
-// Solution 4 (help from ChatGPT)
-const nextPermutation = (nums) => {
-  let i = nums.length - 2;
-
-  while (i >= 0 && nums[i] >= nums[i + 1]) {
-    i--;
-  }
-
-  if (i >= 0) {
-    let j = nums.length - 1;
-    while (nums[j] <= nums[i]) {
-      j--;
-    }
-
-    [nums[i], nums[j]] = [nums[j], nums[i]];
-  }
-
-  let left = i + 1;
-  let right = nums.length - 1;
-  while (left < right) {
-    [nums[left], nums[right]] = [nums[right], nums[left]];
-    left++;
-    right--;
-  }
-
-  return console.log(nums);
-};
-
-nextPermutation([1, 1, 5, 4]);
 
 // Solution 5
 const getIndexOfTarget = (nums, target) => {
