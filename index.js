90bf501c0b060eaa73f7fb1197266b299a52dd24
gandib <55914763+gandib@users.noreply.github.com>
2025-02-09 21:57:22 +0600

    Problems completed

--- index.js
+++ index.js
@@ -15,15 +15,6 @@ const getIndex = (nums, target) => {
 getIndex([1, 4, 5], 6);
 
 // Solution 2
-
-// Input: nums = [-1,0,1,2,-1,-4]
-// Output: [[-1,-1,2],[-1,0,1]]
-// nums[0] + nums[1] + nums[2] = (-1) + 0 + 1 = 0.
-// nums[1] + nums[2] + nums[4] = 0 + 1 + (-1) = 0.
-// nums[0] + nums[3] + nums[4] = (-1) + 2 + (-1) = 0.
-// The distinct triplets are [-1,0,1] and [-1,-1,2].
-// Notice that the order of the output and the order of the triplets does not matter.
-// console.log("-1,0,-1" === "1,0,-1");
 const getTriplets = (nums) => {
   const triplets = [];
   for (let i = 0; i < nums.length; i++) {
