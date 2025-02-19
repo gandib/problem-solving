cfc352a6f4ed1881b53354d36124fb8c8b3bf703
gandib <55914763+gandib@users.noreply.github.com>
2025-02-20 00:00:03 +0600

    task-1 problem-1 completed

--- task-1/problem-1.js
+++ task-1/problem-1.js
@@ -30,3 +30,22 @@ s consists of English letters, digits, symbols and spaces.
 */
 
 // Solution 1
+
+const findLongestSubString = (s) => {
+  let charSet = new Set();
+  let left = 0;
+  let maxLength = 0;
+
+  for (let right = 0; right < s.length; right++) {
+    while (charSet.has(s[right])) {
+      charSet.delete(s[left]);
+      left++;
+    }
+    charSet.add(s[right]);
+    maxLength = Math.max(maxLength, right - left + 1);
+  }
+
+  return maxLength;
+};
+
+console.log(findLongestSubString("pwwkewwk"));
