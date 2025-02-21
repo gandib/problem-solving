a70a66716272afa5971a8e8bebb0ae93661f5d5e
gandib <55914763+gandib@users.noreply.github.com>
2025-02-21 18:21:22 +0600

    task-1 problem-3 completed

--- task-1/problem-3.js
+++ task-1/problem-3.js
@@ -1,5 +1,5 @@
 /*
-PW2.01.07 integer-to-roman
+PW2.01.08 roman-to-integer
 
 Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
 Symbol       Value
@@ -10,36 +10,39 @@ L             50
 C             100
 D             500
 M             1000
-For example, 2 is written as II in Roman numeral, just two one's added together. 12 is written as XII, which is simply X + II. The number 27 is written as XXVII, which is XX + V + II.
+For example, 2 is written as II in Roman numeral, just two ones added together. 12 is written as XII, which is simply X + II. The number 27 is written as XXVII, which is XX + V + II.
 
 Roman numerals are usually written largest to smallest from left to right. However, the numeral for four is not IIII. Instead, the number four is written as IV. Because the one is before the five we subtract it making four. The same principle applies to the number nine, which is written as IX. There are six instances where subtraction is used:
 
 I can be placed before V (5) and X (10) to make 4 and 9. 
 X can be placed before L (50) and C (100) to make 40 and 90. 
 C can be placed before D (500) and M (1000) to make 400 and 900.
-Given an integer, convert it to a roman numeral.
-
+Given a roman numeral, convert it to an integer.
+ 
 Example 1:
-Input: num = 3
-Output: "III"
-Explanation: 3 is represented as 3 ones.
+
+Input: s = "III"
+Output: 3
+Explanation: III = 3.
 Example 2:
 
-Input: num = 58
-Output: "LVIII"
-Explanation: L = 50, V = 5, III = 3.
+Input: s = "LVIII"
+Output: 58
+Explanation: L = 50, V= 5, III = 3.
 Example 3:
 
-Input: num = 1994
-Output: "MCMXCIV"
+Input: s = "MCMXCIV"
+Output: 1994
 Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
  
 Constraints:
-1 <= num <= 3999
+1 <= s.length <= 15
+s contains only the characters ('I', 'V', 'X', 'L', 'C', 'D', 'M').
+It is guaranteed that s is a valid roman numeral in the range [1, 3999]
 */
 
-// Solution 2
-const convertIntToRoman = (num) => {
+// Solution 3
+function romanToInt(s) {
   const romanNumerals = [
     { value: 1000, symbol: "M" },
     { value: 900, symbol: "CM" },
@@ -55,19 +58,20 @@ const convertIntToRoman = (num) => {
     { value: 4, symbol: "IV" },
     { value: 1, symbol: "I" },
   ];
-  let roman = "";
+  let integer = 0;
 
   for (const { value, symbol } of romanNumerals) {
-    while (num >= value) {
-      roman += symbol;
-      num -= value;
-      if (num === 0) {
-        return roman;
+    while (s.startsWith(symbol)) {
+      const newSymble = s.slice(symbol.length);
+      s = newSymble;
+      integer += value;
+      if (s === "") {
+        return integer;
       }
     }
   }
-  return roman;
-};
+  return integer;
+}
 
-const result = convertIntToRoman(1994);
+const result = romanToInt("II");
 console.log(result);
